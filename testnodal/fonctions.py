631032626c65eef8f9b8ae1f5e3ee3b70301a6a6
Van maris Pierre <pierre.vanmaris@etu.enseeiht.fr>
2019-01-23 18:01:09 +0100

    nodal broken

--- testnodal/fonctions.py
+++ testnodal/fonctions.py
@@ -26,19 +26,24 @@ def solveur1D_df(mesh,x,dt,alpha,Nptsx,Lx,residu,T1,T0):
     return temp
 
 
-def solveur1D_md(mesh,dt,residu):
+def solveur1D_md(mesh,x,dx,dt,alpha,Nptsx,Lx,residu,T1,T0):
     """Solves the 1D temperture equation using the nodal approach """
+    t=dt
     temp=np.copy(mesh)
+    temp_tmp=np.copy(mesh)
     residu_i=1
 
     #convergence loop
-    while (residu < residu_i):
+    while (t<500*dt):#(residu < residu_i):
         
         for i in range(1,Nptsx-1): #find the range
             
-            temp_tmp[:,i]=temp[:,i] #find the right model
+            temp_tmp[:,i]=temp[:,i]-alpha*(temp[:,i+1]-temp[:,i-1])*dt/(dx**2) #find the right model
         
+        temp_tmp[:,Nptsx-1]=T0-(T0-T1)*math.erf(0.5*x[Nptsx-1]/math.sqrt(alpha*t))
+ 
         residu_i=max(abs(temp_tmp[0,:]-temp[0,:]))
         temp=np.copy(temp_tmp)
         t=t+dt
-    return
+        
+    return temp
--- testnodal/test_nodal.py
+++ testnodal/test_nodal.py
@@ -19,14 +19,14 @@ Nptsx=20
 Nptsy=2
 
 ##Definition of the constants needed for the calculation##
-T0=20
-T1=30
-#Water
+T0=293
+T1=313
+#Water at 20C
 alpha=0.143*10**(-5)
 conductance=598.03*10**(-3)
 resistance=1/conductance
 #Computation constants
-dt=((Lx**2)/(Nptsx**2))/(2*alpha)
+dt=0.4*((Lx**2)/(Nptsx**2))/(2*alpha)
 residu=10**(-5)
 
 
@@ -35,21 +35,23 @@ residu=10**(-5)
 
 #create arrays
 mesh_df=np.zeros((Nptsy,Nptsx))
+mesh_md=np.copy(mesh_df)
 x=np.linspace(0,Lx,Nptsx)
 y=np.linspace(0,Ly,Nptsy)
+dx=x[1]-x[0]
 
 #Initial values of arrays
 mesh_df[:,:]=T0
 mesh_df[:,0]=T1
 
-
-
+mesh_md[:,:]=T0
+mesh_md[:,0]=T1
 
 ##Solver##
 
 #reference
 temp_df=solveur1D_df(mesh_df,x,dt,alpha,Nptsx,Lx,residu,T1,T0)
-#temp_md=solveur1D_md(mesh,dt,residu)
+temp_md=solveur1D_md(mesh_md,x,dx,dt,alpha,Nptsx,Lx,residu,T1,T0)
 
 
 
@@ -65,9 +67,21 @@ plt.colorbar()
 plt.grid()
 plt.show(block=False)
 
+#2Dplot of the nodal method
+plt.figure()
+
+plt.imshow(temp_md.reshape((Nptsy,Nptsx)),extent=(x.min(), x.max(), y.max(), y.min()), 
+    interpolation='nearest',cmap=cm.gray)
+
+plt.colorbar()
+plt.grid()
+plt.show(block=False)
+
+
 #1D plot to compare the 2 methods
 plt.figure()
 plt.plot(x,temp_df[0,:])
+plt.plot(x,temp_md[0,:])
 plt.xlabel('x')
 plt.ylabel('Temp')
 plt.show()
