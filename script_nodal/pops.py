5efc37365ac4636ecff2d50ea9351230722c1d6c
lrogel <leo.rogel@etu.enseeiht.fr>
2019-02-05 14:16:06 +0100

    creation de classe reservoir

---
+++ script_nodal/pops.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+#Imports
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import math as math
+
+from init import Init
+
+class Reservoir(Init)
+	def __init__(self):
+		Init.__init__(self)
+		
+	
+	def systeme(self):
+		pass
+		
