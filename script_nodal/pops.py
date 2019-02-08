c57ea95906141b2b3489c5627c62577339923bdd
lrogel <leo.rogel@etu.enseeiht.fr>
2019-02-08 13:05:34 +0100

    initsys differentiel

--- script_nodal/pops.py
+++ script_nodal/pops.py
@@ -1,18 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 #Imports
 import numpy as np 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import math as math
+import sys
+LibPath='../../Sujet_29/'
+sys.path.append(LibPath)
+import NumericalProblemClass as PbDef
 
 from init import Init
 
-class Reservoir(Init)
+class Reservoir(Init) :
+
 	def __init__(self):
 		Init.__init__(self)
 		
+	def systeme_init(self, Tinit) :
+		T0=0.
+		Tinit = np.zeros(self.Nptsy*Nptsx)
+		Tinit=T0
+		print
 	
-	def systeme(self):
-		
+	def systeme(self,T, dT_dt ):
+		pass
 		
