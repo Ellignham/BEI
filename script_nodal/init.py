39b9baa0e3b0eeb605fd7ce61b5e4b792b6db1a6
Van maris Pierre <pierre.vanmaris@etu.enseeiht.fr>
2019-02-13 10:59:28 +0100

    voisins

--- script_nodal/init.py
+++ script_nodal/init.py
@@ -28,10 +28,10 @@ class Init(Input):
         Input.__init__(self)
         
         self.x = np.linspace(0,self.Lx/2,self.Nptsx)
-        self.y = np.linspace(0,self.Ly,self.Nptsy)
+        self.y = np.linspace(self.Lx/2,self.Ly-self.Lx/2,self.Nptsy)
 
         self.nodes = np.zeros((self.Nptsx*self.Nptsy+self.Nptsx*self.ntheta,3))
-        self.neig = np.zeros((self.Nptsx*self.Nptsy+self.Nptsx*self.ntheta,5))
+        self.neig = np.zeros((self.Nptsx*self.Nptsy+self.Nptsx*self.ntheta,4+self.ntheta))
 
     def domain_cart(self):
         """
@@ -51,19 +51,17 @@ class Init(Input):
                     self.neig[i+j*self.Nptsx,2]=i+1+j*self.Nptsx
                     self.neig[i+j*self.Nptsx,3]=i+(j-1)*self.Nptsx
                     self.neig[i+j*self.Nptsx,4]=i+(j+1)*self.Nptsx
-          
+                    self.neig[i+j*self.Nptsx,5]=-1 
                 elif (j==0 and i>0 and i<self.Nptsx-1):
                     self.neig[i+j*self.Nptsx,1]=i-1+j*self.Nptsx 
                     self.neig[i+j*self.Nptsx,2]=i+1+j*self.Nptsx
                     self.neig[i+j*self.Nptsx,3]=i+(j+1)*self.Nptsx
                     self.neig[i+j*self.Nptsx,4]=-1
-
                 elif (j==self.Nptsy-1 and i>0 and i<self.Nptsx-1):
                     self.neig[i+j*self.Nptsx,1]=i-1+j*self.Nptsx 
                     self.neig[i+j*self.Nptsx,2]=i+1+j*self.Nptsx
                     self.neig[i+j*self.Nptsx,3]=i+(j-1)*self.Nptsx
                     self.neig[i+j*self.Nptsx,4]=-1
-
                 elif (i==0 and j>0 and j<self.Nptsy-1):
                     self.neig[i+j*self.Nptsx,1]=i+1+j*self.Nptsx 
                     self.neig[i+j*self.Nptsx,2]=i+(j-1)*self.Nptsx
@@ -83,47 +81,161 @@ class Init(Input):
         self.neig[self.Nptsx*self.Nptsy-1,1]=self.Nptsx*self.Nptsy-2; self.neig[self.Nptsx*self.Nptsy-1,2]=self.Nptsx*(self.Nptsy-1)-1;self.neig[self.Nptsx*self.Nptsy-1,3]=-1
     
         
-    def domain(self):
+    def domain_tank(self):
         """
         Creates the shape of the tank and fills it with nodes
-        """
         
+        creation in 3 steps :
+            -creation on the 'rectangle part' of the tank and storage of the neighbors (see domain_cart)
+            -creation of the bottom 'circle part' centered on (Lx,Lx) using polar coordinates
+            -creation of the upper 'circle part' centered on (Ly+Lx,Lx) using polar coordinates
 
-        #Create the boudary of the tank
-        self.boundary=np.zeros((self.Nptsy,2))
+        local variables :
+       
+            id_node : id of the node (used to create the 'cicrle parts') 
+            angle   : angle between two consecutive lines of points for polar coordinats
+
+            r       : distance to the center of the 'circle' for polar coordinates
+            theta   : angle relative to the border of the 'rectangle part' for polar coordinates
+        """
+       
+        assert (self.ntheta > 0), "ntheta must be >0 to use the tank shape" 
+
+        #Create the 'rectangle part' of the tank
 
         for j in range(0,self.Nptsy):
-            self.boundary[j,0]=self.y[j]
-            if (self.y[j]<self.Lx/2):
-                self.boundary[j,1]=self.Lx/2-math.sqrt((self.Lx/2)**2-(self.y[j]-self.Lx/2)**2)
-            elif (self.y[j]>self.Ly-self.Lx/2):
-                self.boundary[j,1]=self.Lx/2-math.sqrt(abs((self.Lx/2)**2-(self.y[j]-self.Ly+self.Lx/2)**2))
-            else :
-                self.boundary[j,1]=0
+            for i in range(0,self.Nptsx):
+                #nodes
+                self.nodes[i+j*self.Nptsx,0]=i+j*self.Nptsx
+                self.nodes[i+j*self.Nptsx,1]=self.y[j]
+                self.nodes[i+j*self.Nptsx,2]=self.x[i]
+                #neighbor
+                self.neig[i+j*self.Nptsx,0]=i+j*self.Nptsx
+                #middle
+                if (j>0 and j<self.Nptsy-1 and i>0 and i<self.Nptsx-1):
+                    self.neig[i+j*self.Nptsx,1]=i-1+j*self.Nptsx 
+                    self.neig[i+j*self.Nptsx,2]=i+1+j*self.Nptsx
+                    self.neig[i+j*self.Nptsx,3]=i+(j-1)*self.Nptsx
+                    self.neig[i+j*self.Nptsx,4]=i+(j+1)*self.Nptsx
+                    self.neig[i+j*self.Nptsx,5]=-1 
+                #lower boudary
+                elif (j==0 and i>0 and i<self.Nptsx-1):
+                    self.neig[i+j*self.Nptsx,1]=i-1+j*self.Nptsx 
+                    self.neig[i+j*self.Nptsx,2]=i+1+j*self.Nptsx
+                    self.neig[i+j*self.Nptsx,3]=i+(j+1)*self.Nptsx
+                    self.neig[i+j*self.Nptsx,4]=-1
+                #upper boundary
+                elif (j==self.Nptsy-1 and i>0 and i<self.Nptsx-1):
+                    self.neig[i+j*self.Nptsx,1]=i-1+j*self.Nptsx 
+                    self.neig[i+j*self.Nptsx,2]=i+1+j*self.Nptsx
+                    self.neig[i+j*self.Nptsx,3]=i+(j-1)*self.Nptsx
+                    self.neig[i+j*self.Nptsx,4]=-1
+                #left boundary
+                elif (i==0 and j>0 and j<self.Nptsy-1):
+                    self.neig[i+j*self.Nptsx,1]=i+1+j*self.Nptsx 
+                    self.neig[i+j*self.Nptsx,2]=i+(j-1)*self.Nptsx
+                    self.neig[i+j*self.Nptsx,3]=i+(j+1)*self.Nptsx
+                    self.neig[i+j*self.Nptsx,4]=-1
+                #right boundary
+                elif (i==self.Nptsx-1 and j>0 and j<self.Nptsy-1):
+                    self.neig[i+j*self.Nptsx,1]=i-1+j*self.Nptsx 
+                    self.neig[i+j*self.Nptsx,2]=i+(j+1)*self.Nptsx
+                    self.neig[i+j*self.Nptsx,3]=i+(j-1)*self.Nptsx
+                    self.neig[i+j*self.Nptsx,4]=-1
+        #corners
+        self.neig[0,1]=1; self.neig[0,2]=self.Nptsx;self.neig[0,3]=-1                            
+        self.neig[self.Nptsx-1,1]=self.Nptsx-2; self.neig[self.Nptsx-1,2]=2*self.Nptsx-1;self.neig[self.Nptsx-1,3]=-1
+      
+        self.neig[self.Nptsx*(self.Nptsy-1),1]=self.Nptsx*(self.Nptsy-1)+1; self.neig[self.Nptsx*(self.Nptsy-1),2]=self.Nptsx*(self.Nptsy-2);self.neig[self.Nptsx*(self.Nptsy-1),3]=-1
+        self.neig[self.Nptsx*self.Nptsy-1,1]=self.Nptsx*self.Nptsy-2; self.neig[self.Nptsx*self.Nptsy-1,2]=self.Nptsx*(self.Nptsy-1)-1;self.neig[self.Nptsx*self.Nptsy-1,3]=-1
+   
+        id_node=self.Nptsy*self.Nptsx-1 
+        #Create lower 'cricle part'
+       
+        angle=math.pi/(self.ntheta*2)
 
-        #Fill the tank with nodes
-        self.xnodes=np.zeros((self.Nptsy,self.Nptsx))
-        self.ynodes=np.zeros((self.Nptsy,self.Nptsx))
-
-        for i in range(0,self.Nptsx):
-            for j in range(0,self.Nptsy):
-                if (self.y[j]<self.Lx/2):# and self.y[j]>math.sqrt(abs((self.Lx/2-self.x[i]/2)**2-(self.y[j]-self.Lx/2)**2))-self.Lx/2 ) :
-                    self.ynodes[j,i]=self.y[j]
-                    self.xnodes[j,i]=self.x[i]
-                elif (self.y[j]<self.Ly-self.Lx/2 and self.y[j]>self.Lx/2):
-                    self.ynodes[j,i]=self.y[j]
-                    self.xnodes[j,i]=self.x[i]
-                elif (self.y[j]>self.Ly+self.Lx/2): 
-                    self.ynodes[j,i]=self.y[j]
-                    self.xnodes[j,i]=self.x[i]#self.Lx/2-math.sqrt(abs((self.Lx/2-self.x[i]/2)**2-(self.y[j]-self.Ly+self.Lx/2)**2))
-
-
-        
-        plt.figure()
-        plt.plot(self.boundary[:,1],self.boundary[:,0],'or')
-        plt.xlim(-0.5,1.5)
-        plt.ylim(-1,1)
-        plt.plot(self.xnodes,self.ynodes,'o')
+        for theta in range(1,self.ntheta+1):
+            for r in range(1,self.Nptsx):
+                #update the id of the node
+                id_node+=1
+                #nodes
+                self.nodes[id_node,0]=id_node
+                self.nodes[id_node,1]=self.Lx/2-self.x[r]*math.sin(theta*angle)
+                self.nodes[id_node,2]=self.Lx/2-self.x[r]*math.cos(theta*angle)
+                #neighbor
+                self.neig[id_node,0]=id_node
+                if (r>1 and r<self.Nptsx-1 and theta>1 and theta<self.ntheta):
+                    self.neig[id_node,1]=id_node-1
+                    self.neig[id_node,2]=id_node+1
+                    self.neig[id_node,3]=id_node+self.Nptsx-1
+                    self.neig[id_node,4]=id_node-self.Nptsx+1
+                    self.neig[id_node,5]=-1
+                #border of the circle
+                elif (r==self.Nptsx-1 and theta>1 and theta<self.ntheta):
+                    self.neig[id_node,1]=id_node-1
+                    self.neig[id_node,2]=id_node+self.Nptsx-1
+                    self.neig[id_node,3]=id_node-self.Nptsx+1
+                    self.neig[id_node,4]=-1
+                #right boundary
+                elif (r>1 and r<self.Nptsx-1 and theta==self.ntheta):
+                    self.neig[id_node,1]=id_node-1
+                    self.neig[id_node,2]=id_node+1
+                    self.neig[id_node,3]=id_node+self.Nptsx-1
+                    self.neig[id_node,4]=-1
+                #upper boundary
+                elif (r>1 and r<self.Nptsx-1 and theta==1 ):
+                    self.neig[id_node,1]=id_node-1
+                    self.neig[id_node,2]=id_node+1
+                    self.neig[id_node,3]=self.Nptsx-1-r
+                    self.neig[id_node,4]=id_node+self.Nptsx-1
+                    self.neig[self.Nptsx-1-r,4]=id_node
+                    self.neig[self.Nptsx-1-r,5]=-1
+                #center of the circle
+                elif (r==1 and theta>1 and theta<self.ntheta):
+                    self.neig[id_node,1]=id_node+1
+                    self.neig[id_node,2]=id_node+self.Nptsx-1
+                    self.neig[id_node,3]=id_node-self.Nptsx+1
+                    self.neig[id_node,4]=self.Nptsx-1
+                    self.neig[id_node,5]=-1
+                    self.neig[self.Nptsx-1,2+theta]=id_node
+                #corners
+                elif (r==self.Nptsx-1 and theta==1):
+                    self.neig[id_node,1]=id_node-1
+                    self.neig[id_node,2]=id_node+self.Nptsx-1
+                    self.neig[id_node,3]=0
+                    self.neig[id_node,4]=-1
+                    self.neig[0,3]=id_node
+                    self.neig[0,4]=-1
+                elif (r==self.Nptsx-1 and theta==self.ntheta):
+                    self.neig[id_node,1]=id_node-1
+                    self.neig[id_node,2]=id_node-self.Nptsx+1
+                    self.neig[id_node,3]=-1
+                elif (r==1 and theta==1):
+                    self.neig[id_node,1]=id_node+1
+                    self.neig[id_node,2]=id_node+self.Nptsx-1
+                    self.neig[id_node,3]=self.Nptsx-2
+                    self.neig[id_node,4]=self.Nptsx-1
+                    self.neig[id_node,5]=-1
+                    self.neig[self.Nptsx-2,4]=id_node
+                    self.neig[self.Nptsx-2,5]=-1
+                    self.neig[self.Nptsx-1,3]=id_node
+                elif (r==1 and theta==self.ntheta):
+                    self.neig[id_node,1]=id_node+1
+                    self.neig[id_node,2]=id_node-self.Nptsx+1
+                    self.neig[id_node,3]=self.Nptsx-1
+                    self.neig[id_node,4]=-1
+                    self.neig[self.Nptsx-1,2+theta]=id_node
+                    self.neig[self.Nptsx-1,3+theta]=-1
+        print(self.nodes)
+        print(self.neig)
+ 
+        fig=plt.figure()
+        ax = fig.add_subplot(111)
+        plt.plot(self.nodes[:,2],self.nodes[:,1],'or')
+        for i, txt in enumerate(self.nodes[:,0]):
+            ax.annotate(txt, (self.nodes[i,2], self.nodes[i,1]))
+        plt.xlim(0,0.5)
+        plt.ylim(0,1)
       #  plt.ylim(8,10.5)
       #  plt.xlim(0.5,1.4)
         plt.show()
@@ -194,4 +306,5 @@ class Init(Input):
 
 
 
-
+test=Init()
+test.domain_tank()
