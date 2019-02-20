8877de209eea6643ac11fee5e771568735644b5c
Van maris Pierre <pierre.vanmaris@etu.enseeiht.fr>
2019-02-20 20:52:25 +0100

    nothing changed but fixed anyway

--- script_nodal/init.py
+++ script_nodal/init.py
@@ -344,7 +344,7 @@ class Init(Input):
         for i, txt in enumerate(self.nodes[:,0]):
             ax.annotate(txt, (self.nodes[i,2], self.nodes[i,1]))
         plt.xlim(0,0.5)
-        plt.ylim(0,0.5)
+        plt.ylim(9.5,10)
       #  plt.ylim(8,10.5)
       #  plt.xlim(0.5,1.4)
         plt.show()
