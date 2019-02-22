cdb2879018a8b66b199a139a9543a84491a23f9e
Chapelet Gaspard <gchapele@localhost.localdomain>
2019-02-22 15:13:31 +0100

    ecriturecsv

--- script_nodal/visu.py
+++ script_nodal/visu.py
@@ -116,23 +116,34 @@ def plot_pres(self):
 	plt.ylabel('y')
 	plt.title('Pressure')
 
-def ecriture_csv(ProblemSize,temps,Reservoire):
+def ecriture_csv(ProblemSize,temps,Reservoir):
 	"""
 	Reads the array for each time
 	return an array of the field, and temperature for each coordinate
 	"""
+
 	ArrayTemp=np.zeros((ProblemSize,4),dtype=float)
 	liste=["x","y","z","Temperature"]
-	for j in range(0,len(temps)):
-		f=open("ArrayTemp.csv.{}".format(j),"wb")
-		ArrayTemp[:,0]=Reservoire.domain[:,0]
-		ArrayTemp[:,1]=Reservoire.domain[:,1]
-		ArrayTemp[:,2]=0
-		ArrayTemp[:,3]=temp[j]
-		writer=csv.writer(f,delimiter=',')
-		writer.writerow(liste)
-		writer.writerows(ArrayTemp)
-
+	if(Reservoir.mesh_type=='cart'):
+		for j in range(0,len(temps)):
+			f=open("ArrayTemp_cart/ArrayTemp_cart.csv.{}".format(j),"wb")
+			ArrayTemp[:,0]=Reservoir.nodes[:,2]
+			ArrayTemp[:,1]=Reservoir.nodes[:,1]
+			ArrayTemp[:,2]=0
+			ArrayTemp[:,3]=temp[j]
+			writer=csv.writer(f,delimiter=',')
+			writer.writerow(liste)
+			writer.writerows(ArrayTemp)
+	else:
+		for j in range(0,len(temps)):
+			f=open("ArrayTemp_tank/ArrayTemp_tank.csv.{}".format(j),"wb")
+			ArrayTemp[:,0]=Reservoir.nodes[:,2]
+			ArrayTemp[:,1]=Reservoir.nodes[:,1]
+			ArrayTemp[:,2]=0
+			ArrayTemp[:,3]=temp[j]
+			writer=csv.writer(f,delimiter=',')
+			writer.writerow(liste)
+			writer.writerows(ArrayTemp)
 
 
 #~ vmax=abs(zi).max(), vmin=-abs(zi).max(),
